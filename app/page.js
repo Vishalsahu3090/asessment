11c89e207d62de9c9b56320077c296e6aed7ac7e
Singh-Sg <developer@example.com>
2025-12-30 23:49:57 +0530

    folder structure change

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <h1>Root Layout Page</h1>;
+import PokemonTable from "../app/poke/PokemonTable";
+
+export default function PokePage() {
+  return (
+    <div className="min-h-screen p-6">
+      <h1 className="text-2xl font-semibold mb-4">Pokémon List</h1>
+      <PokemonTable />
+    </div>
+  );
 }
